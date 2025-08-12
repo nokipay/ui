e53a444b250b5e6ad76b105e88a716c3f33b15ff
reille-B <reillekiwa@gmail.com>
2025-08-12 12:59:54 +0100

    fix

--- dist/module.d.ts
+++
@@ -1,6 +0,0 @@
-interface ModuleOptions {
-}
-declare const _default: any;
-
-export { _default as default };
-export type { ModuleOptions };
--- dist/module.mjs
+++ dist/module.mjs
@@ -1,22 +1,18 @@
-import { defineNuxtModule, createResolver, installModule, addComponentsDir, addPlugin } from '@nuxt/kit';
+import { createJiti } from "C:/Users/hp/Documents/Present/Nokipay/ui/node_modules/jiti/lib/jiti.mjs";
 
-const module = defineNuxtModule({
-  meta: {
-    name: "@nokipay/ui",
-    configKey: "uiLibrary"
+const jiti = createJiti(import.meta.url, {
+  "interopDefault": true,
+  "alias": {
+    "@nokipay/ui": "C:/Users/hp/Documents/Present/Nokipay/ui"
   },
-  defaults: {},
-  async setup(options, nuxt) {
-    const resolver = createResolver(import.meta.url);
-    await installModule("@nuxt/ui");
-    addComponentsDir({
-      path: resolver.resolve("./runtime/components"),
-      pathPrefix: true,
-      prefix: "Noki",
-      global: true
-    });
-    addPlugin(resolver.resolve("./runtime/plugin"));
+  "transformOptions": {
+    "babel": {
+      "plugins": []
+    }
   }
-});
+})
 
-export { module as default };
+/** @type {import("C:/Users/hp/Documents/Present/Nokipay/ui/src/module.js")} */
+const _module = await jiti.import("C:/Users/hp/Documents/Present/Nokipay/ui/src/module.ts");
+
+export default _module?.default ?? _module;
